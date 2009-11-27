3695bb2538ae0adb80164b2618b6a718ad2526f5
Alecs King <alecsk@gmail.com>
2009-11-27 17:13:15 +0800

    add a simple expr evalator

---
+++ simexpr.go
@@ -0,0 +1,32 @@
+// evaluate simple single expression
+// 
+// demo use of exp/eval
+// basic input/output
+package main
+
+import (
+	"fmt";
+	"exp/eval";
+	"os";
+	"bufio";
+)
+
+func main() {
+	w := eval.NewWorld();
+	r := bufio.NewReader(os.Stdin);
+	for {
+		s, e := r.ReadString('\n');
+		if e == os.EOF {
+			break
+		}
+		c, err := w.Compile(s);
+		if err == nil {
+			v, e := c.Run();
+			if e == nil && v != nil {	// single expression
+				fmt.Println(v)
+			}
+		} else {
+			fmt.Println("compile error");
+		}
+	}
+}
