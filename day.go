060f8c680ee769e25c8596329879b6e187ace86d
Alecs King <alecsk@gmail.com>
2009-11-27 18:29:06 +0800

    add a day.go written the other day

---
+++ day.go
@@ -0,0 +1,29 @@
+// slice, method, stringer, typed const, etc..
+package main
+
+import "fmt"
+
+type Day int
+
+// := only can be used inside a function
+// it cannot be used in top-level declaration
+var dayName = []string { "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur", "Sun" }
+
+func (day Day) String() string {
+	return dayName[day] + "day";
+}
+
+const (
+    Monday Day = iota;
+    Tuesday;
+    Wednesday;
+    Thursday;
+    Friday;
+    Saturday;
+    Sunday;
+    )
+
+func main() {
+	day := Thursday;
+	fmt.Println(day)
+}
