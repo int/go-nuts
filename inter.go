983264d68174362faadf03a188cdd4702c20c710
Alecs King <alecsk@gmail.com>
2009-11-29 23:05:58 +0800

    test interfaces and methods: function as reciever and embedding

---
+++ inter.go
@@ -0,0 +1,32 @@
+// test interfaces and methods
+// * function as reciever of method
+// * embedding
+
+package main
+
+type Interface interface {
+	Foo(int) int;
+}
+
+type Int int
+
+func (p Int) Foo(a int) int	{ return int(p) + a }
+
+type Struct struct {
+	Int;
+}
+
+type Func func(byte)
+
+func (f Func) Foo(a int) int {
+	f('x');
+	return a * 2;
+}
+
+func pc(c byte)	{ println(c) }
+
+func main() {
+	s := Struct{7};
+	println((&s).Foo(8));	// value receiver works for pointer
+	println(Func(pc).Foo(3));
+}
